1fb1e5a1566c1ce5c3e175c1888ea5188525d8d8
Aleksandr Belous <belous.a.a.h@yandex.ru>
2026-09-06 22:09:07 +0300

    Добавлена логика обработки ответа сервера.

--- xakep-ru/2026-03-06_go-port-scanner/main.go
+++ xakep-ru/2026-03-06_go-port-scanner/main.go
@@ -7,6 +7,18 @@ import (
 	"time"
 )
 
+func grabBanner(conn net.Conn, buf []byte) (string, bool) {
+	request := fmt.Sprintf("HEAD / HTTP/1.1\r\nHost: %s\r\n\r\n", conn.RemoteAddr())
+	conn.Write([]byte(request))
+
+	n, err := conn.Read(buf)
+	if err == nil && n > 0 {
+		return string(buf[:n]), true
+	}
+
+	return "", false
+}
+
 func main() {
 	if len(os.Args) <= 1 {
 		fmt.Fprintf(os.Stderr, "Target address not specified\n")
@@ -22,15 +34,25 @@ func main() {
 
 	ports := []string{"21", "22", "25", "53", "80", "8080", "443", "110", "143", "3306", "3389"}
 	timeout := 250 * time.Millisecond
+	buffer := make([]byte, 256)
 
 	for _, port := range ports {
 		addr := net.JoinHostPort(target, port)
+
 		conn, err := net.DialTimeout("tcp", addr, timeout)
 		if err != nil {
 			continue
 		}
-		conn.Close()
+
 		fmt.Printf("Port %s is open\n", port)
+
+		deadline := time.Now().Add(timeout)
+		conn.SetDeadline(deadline)
+
+		s, isOk := grabBanner(conn, buffer)
+		if isOk {
+			fmt.Println(s)
+		}
 	}
 
 	os.Exit(0)
