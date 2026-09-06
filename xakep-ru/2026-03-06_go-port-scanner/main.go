3dce79f49a2bf799890152df000760f332860f01
Aleksandr Belous <belous.a.a.h@yandex.ru>
2026-09-06 22:45:09 +0300

    Логика скрипта переведена на паттерн main-run.

--- xakep-ru/2026-03-06_go-port-scanner/main.go
+++ xakep-ru/2026-03-06_go-port-scanner/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"time"
 )
+
+func loadPortsList(fileName string) ([]string, error) {
+	var ports []string
+
+	prtFile, err := os.Open(fileName)
+	if err != nil {
+		return ports, err
+	}
+	defer prtFile.Close()
+
+	scanner := bufio.NewScanner(prtFile)
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		_, err := strconv.ParseUint(s, 10, 16)
+		if err != nil {
+			continue
+		}
+		ports = append(ports, s)
+	}
+
+	err = scanner.Err()
+	return ports, err
+}
 
 func grabBanner(conn net.Conn, buf []byte) (string, bool) {
 	request := fmt.Sprintf("HEAD / HTTP/1.1\r\nHost: %s\r\n\r\n", conn.RemoteAddr())
@@ -19,41 +46,87 @@ func grabBanner(conn net.Conn, buf []byte) (string, bool) {
 	return "", false
 }
 
-func main() {
-	if len(os.Args) <= 1 {
-		fmt.Fprintf(os.Stderr, "Target address not specified\n")
-		os.Exit(1)
+func run() (err error) {
+	const (
+		srcFileName = "targets.txt"
+		prtFileName = "ports.txt"
+		dstFileName = "results.txt"
+	)
+
+	ports, err := loadPortsList(prtFileName)
+	if err != nil {
+		return err
+	}
+	if len(ports) == 0 {
+		e := fmt.Sprintf("errpr reading %s: empty file\n", prtFileName)
+		return errors.New(e)
 	}
 
-	target := os.Args[1]
-
-	if net.ParseIP(target) == nil {
-		fmt.Fprintf(os.Stderr, "%s is not a valid address\n", target)
-		os.Exit(1)
+	srcFile, err := os.Open(srcFileName)
+	if err != nil {
+		return err
 	}
+	defer srcFile.Close()
 
-	ports := []string{"21", "22", "25", "53", "80", "8080", "443", "110", "143", "3306", "3389"}
+	scanner := bufio.NewScanner(srcFile)
+
+	dstFile, err := os.Open(dstFileName)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	writer := bufio.NewWriter(dstFile)
+
+	defer func() {
+		// Планируем сброс буфера и объединяем ошибки
+		flushErr := writer.Flush()
+		err = errors.Join(err, flushErr)
+	}()
+
 	timeout := 250 * time.Millisecond
 	buffer := make([]byte, 256)
 
-	for _, port := range ports {
-		addr := net.JoinHostPort(target, port)
+	for scanner.Scan() {
+		targetHost := scanner.Text()
 
-		conn, err := net.DialTimeout("tcp", addr, timeout)
-		if err != nil {
-			continue
+		if net.ParseIP(targetHost) == nil {
+			ips, err := net.LookupHost(targetHost)
+			if err != nil || len(ips) == 0 {
+				continue
+			}
+			targetHost = ips[0]
 		}
 
-		fmt.Printf("Port %s is open\n", port)
+		for _, port := range ports {
+			addr := net.JoinHostPort(targetHost, port)
 
-		deadline := time.Now().Add(timeout)
-		conn.SetDeadline(deadline)
+			conn, err := net.DialTimeout("tcp", addr, timeout)
+			if err != nil {
+				continue
+			}
 
-		s, isOk := grabBanner(conn, buffer)
-		if isOk {
-			fmt.Println(s)
+			s := fmt.Sprintf("%s is open\n", addr)
+			fmt.Print(s)
+			writer.WriteString(s)
+
+			deadline := time.Now().Add(timeout)
+			conn.SetDeadline(deadline)
+
+			banner, _ := grabBanner(conn, buffer)
+			fmt.Println(banner)
+			writer.WriteString(banner)
+
+			conn.Close()
 		}
 	}
 
-	os.Exit(0)
+	return scanner.Err()
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
