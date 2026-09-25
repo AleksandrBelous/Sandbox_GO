15e74f01f4071a8905be9d610fdf998e20cee006
Aleksandr Belous <belous.a.a.g@yandex.ru>
2026-09-25 15:18:45 +0300

    Изменён дизайн передачи хешей - реализована передача пакетов хешей для анализа в обработчики. Профилирование через tool pprof.

--- xakep-ru/2026-07-16_go-hashcracking/main.go
+++ xakep-ru/2026-07-16_go-hashcracking/main.go
@@ -18,12 +18,14 @@ import (
 )
 
 type PipelineConfig struct {
-	JobCh       chan string
-	ResultCh    chan string
-	ErrorCh     chan<- error
-	SrcFileName string
-	TargetHash  *[md5.Size]byte
-	Counter     *atomic.Uint64
+	JobCh               chan []string
+	ResultCh            chan string
+	ErrorCh             chan<- error
+	SrcFileName         string
+	TargetHash          *[md5.Size]byte
+	Counter             *atomic.Uint64
+	CancelCheckInterval int
+	BatchSize           int
 }
 
 // produce генерирует задания для обработки, считывая значения из cfg.SrcFileName, и помещает их в канал cfg.JobCh;
@@ -36,24 +38,58 @@ func produce(ctx context.Context, cfg *PipelineConfig) {
 	}
 	defer file.Close()
 
+	// Пакет-задание, содержащий строки для обработки
+	batch := make([]string, 0, cfg.BatchSize)
+	// Счётчик считанных строк
+	i := 0
+	// Неблокирующая отправка пакета в канал cfg.JobCh:
+	// возвращает true при успешном завершении записи
+	// или false, если произошла отмена контекста
+	sendBatch := func(batch []string) bool {
+		select {
+		case <-ctx.Done():
+			return false
+		case cfg.JobCh <- batch:
+			return true
+		}
+	}
+
+	// Считываем файл построчно
 	scanner := bufio.NewScanner(file)
+	defer func() {
+		if err := scanner.Err(); err != nil {
+			cfg.ErrorCh <- fmt.Errorf("reading %s: %w", cfg.SrcFileName, err)
+		}
+	}()
 	for scanner.Scan() {
+		// Периодически проверяем необходимость отмены
+		if i%cfg.CancelCheckInterval == 0 {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+		}
+		i++
+
+		// Формируем пакет добавлением строк
 		s := strings.TrimSpace(scanner.Text())
 		if s == "" {
 			continue
 		}
+		batch = append(batch, s)
 
-		select {
-		case <-ctx.Done():
-			//
-			return
-		case cfg.JobCh <- s:
-			// Или отправка в канал разблокируется и мы перейдем к следующей итерации цикла чтения
+		// По достижении заданного размера отправляем задание
+		if len(batch) == cfg.BatchSize {
+			if sendBatch(batch) {
+				// После отправки формируем новый пакет
+				batch = make([]string, 0, cfg.BatchSize)
+			}
 		}
 	}
-
-	if err := scanner.Err(); err != nil {
-		cfg.ErrorCh <- fmt.Errorf("reading %s: %w", cfg.SrcFileName, err)
+	// Отправляем оставшийся неполный пакет перед выходом
+	if len(batch) > 0 {
+		sendBatch(batch)
 	}
 }
 
@@ -63,16 +99,32 @@ func produce(ctx context.Context, cfg *PipelineConfig) {
 func worker(ctx context.Context, cfg *PipelineConfig) {
 	for {
 		select {
-		case job, ok := <-cfg.JobCh:
+		case jobs, ok := <-cfg.JobCh:
 			if !ok {
 				// Возврат при чтении из закрытого канала, проверка факта закрытия по второму значению (ok)
 				return
 			}
-			// Увеличиваем счётчик попыток сравнения для каждого сравнения
-			cfg.Counter.Add(1)
-			if md5.Sum([]byte(job)) == *cfg.TargetHash {
-				cfg.ResultCh <- job
-				return
+			for i, job := range jobs {
+				if i%cfg.CancelCheckInterval == 0 {
+					// Неблокирующая проверка отмены контекста: если отменен, немедленно выходим;
+					// в противном случае продолжаем
+					select {
+					case <-ctx.Done():
+						return
+					default:
+					}
+				}
+				// Увеличиваем счётчик попыток сравнения для каждого сравнения
+				cfg.Counter.Add(1)
+				if md5.Sum([]byte(job)) == *cfg.TargetHash {
+					// Одновременно ждем отмены контекста или записи в канал;
+					// после выполнения первого из этих действий переходим к return
+					select {
+					case cfg.ResultCh <- job:
+					case <-ctx.Done():
+					}
+					return
+				}
 			}
 		case <-ctx.Done():
 			//
@@ -129,7 +181,7 @@ func main() {
 	}
 
 	// Канал с заданиями
-	jobCh := make(chan string, maxWorkers)
+	jobCh := make(chan []string, maxWorkers)
 	// Канал с результатами
 	resultCh := make(chan string)
 	// Канал с ошибками
@@ -142,12 +194,14 @@ func main() {
 	defer stop()
 
 	config := &PipelineConfig{
-		JobCh:       jobCh,
-		ResultCh:    resultCh,
-		ErrorCh:     errCh,
-		SrcFileName: srcFileName,
-		TargetHash:  &hashBytes,
-		Counter:     &count,
+		JobCh:               jobCh,
+		ResultCh:            resultCh,
+		ErrorCh:             errCh,
+		SrcFileName:         srcFileName,
+		TargetHash:          &hashBytes,
+		Counter:             &count,
+		CancelCheckInterval: 64,
+		BatchSize:           1024,
 	}
 
 	// Отдельная горутина для вывода попыток через равные промежутки времени
